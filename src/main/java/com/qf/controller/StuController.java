86b9748db0ee28f0b801626ef95aa5ebfa1d3db3
xxxijesse <513871545@qq.com>
2019-11-28 20:46:40 +0800

    第一次提交

---
+++ src/main/java/com/qf/controller/StuController.java
@@ -0,0 +1,55 @@
+package com.qf.controller;
+
+import com.qf.entity.Student;
+import com.qf.service.IStuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/stu")
+public class StuController {
+
+    @Autowired
+    private IStuService stuService;
+
+    @RequestMapping("/list")
+    public String list(Model model){
+        List<Student> list = stuService.list();
+        model.addAttribute("stus",list);
+        return "stulist";
+    }
+
+    @RequestMapping("/delete")
+    public String delete(Student student){
+        stuService.removeById(student.getId());
+        return "redirect:/stu/list";
+    }
+
+    @RequestMapping("/toAdd")
+    public String toAdd(){
+        return "add";
+    }
+
+    @RequestMapping("/add")
+    public String add(Student student){
+        stuService.save(student);
+        return "index";
+    }
+
+    @RequestMapping("/toUpdate")
+    public String toUpdate(Student student,Model model){
+        Student student1=stuService.getById(student.getId());
+        model.addAttribute("student",student1);
+        return "update";
+    }
+
+    @RequestMapping("update")
+    public String update(Student student){
+        stuService.updateById(student);
+        return "redirect:/stu/list";
+    }
+}
---
+++ src/main/java/com/qf/service/IStuService.java
@@ -0,0 +1,7 @@
+package com.qf.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.qf.entity.Student;
+
+public interface IStuService extends IService<Student> {
+}
